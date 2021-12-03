3eab19bebd71b7e547ebbf0ad4f51b14168a3839
phill@hallambaker.com <phill@hallambaker.com>
2021-12-03 11:20:20 -0500

    Add project files.

---
+++ HypotheticalBrowser/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HypotheticalBrowser;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        }
+
+    private void ButtonGo_Click(object sender, RoutedEventArgs e) {
+        if (webView != null && webView.CoreWebView2 != null) {
+            webView.CoreWebView2.Navigate(addressBar.Text);
+            }
+        }
+
+    private void ButtonReload_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+    private void ButtonHome_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+    private void ButtonSecure_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+    private void ButtonDownload_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+    private void ButtonBookmark_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+    private void ButtonSettings_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
